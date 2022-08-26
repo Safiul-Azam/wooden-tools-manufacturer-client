22b9cc60750fb65c3cb4fd0eda82419c90c24b81
Safiul-Azam <safiulazamriad@gmail.com>
2022-08-26 10:39:50 +0600

    some changed in site

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 import { Link, useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import { toast } from 'react-toastify';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -78,50 +78,55 @@ const Purchase = () => {
         return <Loading></Loading>
     }
     return (
-        <div className='container mx-auto grid grid-cols-1 lg:grid-cols-3 mt-24 gap-8'>
-            <div>
-                <img className='w-full border shadow-lg' src={handTool.img} alt="" />
-            </div>
-            <div>
-                <h3 className='text-1xl text-secondary font-semibold mb-4'>Factory directly sales <span className='text-primary text-2xl'> {handTool.productName}</span></h3>
-                <hr className='my-4' />
-                <h4 className='text-4xl text-primary mb-4'>Price: ${handTool.perPrice}/<span className='text-lg text-secondary'>Per piece</span></h4>
-                <hr className='mb-4' />
-                <p className='text-xl font-semibold'>Quantity: {handTool.minQuantity}-{handTool.maxQuantity} <span className='text-sm text-secondary'>Pieces</span></p>
-                <hr className='my-4' />
+        <div className='container mx-auto mt-28'>
+            <h2 className="text-4xl mb-10 text-secondary text-center font-bold">Purchase Page</h2>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
+                <div>
+                    <img className='w-full border shadow-lg' src={handTool.img} alt="" />
+                </div>
+                <div>
+                    <h3 className='text-xl text-secondary font-semibold mb-2'>Factory directly sales <span className='text-primary text-2xl'> {handTool.productName}</span></h3>
+                    <hr className='my-2' />
+                    <p>{handTool.description}</p>
+                    <hr className='my-2' />
+                    <h4 className='text-3xl text-primary mb-4'>Price: ${handTool.perPrice}/<span className='text-lg text-secondary'>Per piece</span></h4>
+                    <hr className='mb-4' />
+                    <p className='text-xl font-semibold'>Quantity: {handTool.minQuantity}-{handTool.maxQuantity} <span className='text-sm text-secondary'>Pieces</span></p>
+                    <hr className='my-2' />
 
-                <h3 className='text-xl mb-4 text-primary font-bold text-center   '>Manage Your Quantity</h3>
-                <div className='text-center mb-8 flex'>
-                    <button onClick={handleDecrease} className='btn-md btn rounded-r-none'><FontAwesomeIcon icon={faMinus} /></button>
-                    <input ref={inputRef} className='border w-3/4 text-center input-bordered input rounded-none text-lg' placeholder={handTool.minQuantity} value={handTool.minQuantity} />
-                    <button onClick={handleIncrease} className='btn-md btn rounded-l-none'><FontAwesomeIcon icon={faPlus} /></button>
+                    <h3 className='text-xl mb-4 text-primary font-bold text-center   '>Manage Your Quantity</h3>
+                    <div className='text-center mb-8 flex'>
+                        <button onClick={handleDecrease} className='btn-md btn rounded-r-none'><FontAwesomeIcon icon={faMinus} /></button>
+                        <input ref={inputRef} className='border w-3/4 text-center input-bordered input rounded-none text-lg' placeholder={handTool.minQuantity} value={handTool.minQuantity} />
+                        <button onClick={handleIncrease} className='btn-md btn rounded-l-none'><FontAwesomeIcon icon={faPlus} /></button>
+                    </div>
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input
+                            {...register("name")}
+                            type="text" value={user.displayName || ''} readOnly className="input input-bordered w-full mb-3" />
+                        <input
+                            {...register("email")}
+                            type="text" value={user.email || ''} readOnly className="input input-bordered w-full mb-3" />
+                        <input
+                            {...register("phone")}
+                            required
+                            type="number" placeholder="Phone" className="input input-bordered w-full mb-3" />
+                        <input
+                            {...register("address")}
+                            required
+                            type="text" placeholder="Your Address" className="input input-bordered w-full mb-3" />
+                        <input className='btn btn-secondary w-full' type="submit" value='submit' />
+                    </form>
+                    <div className='w-full text-center'>
+                        <Link to='/dashboard/myOrders'><button className='btn btn-primary w-full mt-4'>Proceed CheckOut</button></Link>
+                        <button className='btn btn-primary btn-sm rounded-full bg-orange-600 text-white w-1/2 my-4'><FontAwesomeIcon className='mr-2 text-lg font-normal' icon={faEnvelope} />Contact Supplier</button>
+                    </div>
                 </div>
             </div>
-            <div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        {...register("name")}
-                        type="text" value={user.displayName || ''} readOnly className="input input-bordered w-full mb-3" />
-                    <input
-                        {...register("email")}
-                        type="text" value={user.email || ''} readOnly className="input input-bordered w-full mb-3" />
-                    <input
-                        {...register("phone")}
-                        required
-                        type="number" placeholder="Phone" className="input input-bordered w-full mb-3" />
-                    <input
-                        {...register("address")}
-                        required
-                        type="text" placeholder="Your Address" className="input input-bordered w-full mb-3" />
-                    <input className='btn btn-secondary w-full' type="submit" value='submit' />
-                </form>
-                <div className='w-full text-center'>
-                    <Link to='/dashboard/myOrders'><button className='btn btn-primary w-full mt-4'>Proceed CheckOut</button></Link>
-                    <button className='btn btn-primary btn-sm rounded-full bg-orange-600 text-white w-1/2 my-4'><FontAwesomeIcon className='mr-2 text-lg font-normal' icon={faEnvelope} />Contact Supplier</button>
-                </div>
-            </div>
+
         </div>
-
     );
 };
 
